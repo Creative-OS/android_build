VoltageOS-staging/build

build: Show useful information after flashing [SQUASH]
* Credits to zephiK and Chroma for the original code
* Fixed Spacing
* Used krexus-caf/build@5c602f2 commit from krexus,

* Citrus-CAF Edits:
  * Show device name next to ROM's name
  * Fix for Devices without ro.product.model prop from Citrus-CAF/build@cdcb576
  * Adapted to Android 9 PIE GetProperty logic from PixelExperience-P/build@800d29c

Signed-off-by: Dmitrii <bankersenator@gmail.com>

tools/releasetools/non_ab_ota.py
@@ -220,6 +220,24 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   # Dump fingerprints
   script.Print("Target: {}".format(target_info.fingerprint))
 
+  date = target_info.GetBuildProp("ro.voltage.build.date")
+  version = target_info.GetBuildProp("ro.voltage.version")
+
+  if target_info.GetBuildProp("ro.product.model") is not None:
+    model = target_info.GetBuildProp("ro.product.model")
+    script.Print("***********************************************");
+    script.Print("           VOLTAGE-OS for %s"%(model));
+    script.Print("   Version: %s"%(version));
+    script.Print("   Compiled on: %s"%(date));
+    script.Print("***********************************************");
+  else:
+    name = target_info.GetBuildProp("ro.product.name")
+    script.Print("***********************************************");
+    script.Print("           VOLTAGE-OS for %s"%(name));
+    script.Print("   Version: %s"%(version));
+    script.Print("   Compiled on: %s"%(date));
+    script.Print("***********************************************");
+
   script.AppendExtra("ifelse(is_mounted(\"/system\"), unmount(\"/system\"));")
   device_specific.FullOTA_InstallBegin()
 

Creative-OS/android_build

Change-Id: I124d1b30f9d85aa169c2ebdc7db96459475409e8

tools/releasetools/non_ab_ota.py
@@ -211,6 +211,24 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   # Dump fingerprints
   script.Print("Target: {}".format(target_info.fingerprint))
 
+  date = target_info.GetBuildProp("ro.creative.build.date")
+  version = target_info.GetBuildProp("ro.creative.version")
+
+  if target_info.GetBuildProp("ro.product.model") is not None:
+    model = target_info.GetBuildProp("ro.product.model")
+    script.Print("***********************************************");
+    script.Print("           CREATIVE-OS for %s"%(model));
+    script.Print("   Version: %s"%(version));
+    script.Print("   Compiled on: %s"%(date));
+    script.Print("***********************************************");
+  else:
+    name = target_info.GetBuildProp("ro.product.name")
+    script.Print("***********************************************");
+    script.Print("           CREATIVE-OS for %s"%(name));
+    script.Print("   Version: %s"%(version));
+    script.Print("   Compiled on: %s"%(date));
+    script.Print("***********************************************");
+
   script.AppendExtra("ifelse(is_mounted(\"/system\"), unmount(\"/system\"));")
   device_specific.FullOTA_InstallBegin()
 